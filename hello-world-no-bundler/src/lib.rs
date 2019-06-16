a1a3fdb48e6be366c728e1fbd996b0a56c2e59d6
Christophe Rosset <tophe@topheman.com>
2019-06-16 17:11:26 +0200

    feat(*): only alert

--- hello-world-no-bundler/src/lib.rs
+++ hello-world-no-bundler/src/lib.rs
@@ -6,16 +6,9 @@ use wasm_bindgen::prelude::*;
 extern "C" {
     #[wasm_bindgen]
     fn alert(message: String);
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(message: String);
 }
 
 #[wasm_bindgen(js_name=alertHelloWorld)]
 pub fn alert_hello_world () {
     alert(format!("Hello World"));
-}
-
-#[wasm_bindgen(js_name=logHelloWorld)]
-pub fn log_hello_world () {
-    log(format!("Hello World"));
 }
